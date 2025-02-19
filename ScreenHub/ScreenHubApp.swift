c5884c433f6258798d33ed1d08aa21c69982bb4e
Jeiel <jeiellima@Jeiels-MacBook-Pro.local>
2025-02-19 11:37:26 -0300

    add initial project

---
+++ ScreenHub/ScreenHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenHubApp.swift
+//  ScreenHub
+//
+//  Created by Jeiel on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
